0dd3c63adc50a0eccef95d9729051a45b83a87bd
hiroya8649 <happy8649@gmail.com>
2019-06-21 10:34:18 +0900

    check state before local storage in privateroute

--- assets/static/auth/component/PrivateRoute.jsx
+++ assets/static/auth/component/PrivateRoute.jsx
@@ -3,6 +3,9 @@ import { Route, Redirect } from 'react-router';
 import store from '@/common/js/store';
 
 function getAuthentication() {
+  if (store.store.getState().auth.accessToken) {
+    return true;
+  }
   const token = localStorage.getItem('access_token');
   if (token) {
     store.store.dispatch({ type: 'LOGIN_SUCCESS', payload: { access_token: token } });
